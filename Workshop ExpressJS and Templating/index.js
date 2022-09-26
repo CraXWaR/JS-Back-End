d4b1f17a2627f0edaab62331377e791004f61ba7
unknown <craxwar@gmail.com>
2022-09-26 15:24:17 +0300

    Completed workshop with some help

--- Workshop ExpressJS and Templating/index.js
+++ Workshop ExpressJS and Templating/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
+const port = 3000;
+
+const homeController = require('./controllers/homePageController');
+const aboutPageController = require('./controllers/aboutPageController');
+const detailsPageController = require('./controllers/detailsPageController');
+const createPageController = require('./controllers/createPageController');
+const pageNotFoundController = require('./controllers/pageNotFoundController');
 
 const app = express();
 
@@ -11,4 +18,10 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
 
-app.listen(3000, () => console.log('hi listening on 3000k port'))
+app.use(homeController);
+app.use('/about', aboutPageController);
+app.use('/details', detailsPageController);
+app.use('/create', createPageController);
+app.use('*/', pageNotFoundController);
+
+app.listen(3000, () => console.log(`Server listening on port ${port}`));
