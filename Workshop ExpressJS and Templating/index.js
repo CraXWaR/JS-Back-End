8f54c5d5bd95685b5aafedbdb0d4c003c69245f5
unknown <craxwar@gmail.com>
2022-10-07 12:57:58 +0300

    adding database and routes

--- Workshop ExpressJS and Templating/index.js
+++ Workshop ExpressJS and Templating/index.js
@@ -1,27 +1,24 @@
 const express = require('express');
-const hbs = require('express-handlebars').create({
-    extname: '.hbs'
-});
-const port = 3000;
-
-const homeController = require('./controllers/homePageController');
-const aboutPageController = require('./controllers/aboutPageController');
-const detailsPageController = require('./controllers/detailsPageController');
-const createPageController = require('./controllers/createPageController');
-const pageNotFoundController = require('./controllers/pageNotFoundController');
-
+const cookieParser = require('cookie-parser');
+const initDatabase = require('./config/database');
+const authMiddleware = require('./middleware/authMiddleware');
+const routes = require('./routes');
 const app = express();
+const PORT = 3000;
 
-app.engine('.hbs', hbs.engine);
-app.set('view engine', '.hbs');
+require('./config/handlebars')(app);
+app.use(express.urlencoded({extended: true}));
 
-app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
+app.use(cookieParser());
+app.use(authMiddleware);
+app.use(routes);
 
-app.use(homeController);
-app.use('/about', aboutPageController);
-app.use('/details', detailsPageController);
-app.use('/create', createPageController);
-app.use('*/', pageNotFoundController);
-
-app.listen(3000, () => console.log(`Server listening on port ${port}`));
+//Set database and run server
+initDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+    })
+    .catch(err => {
+        console.log(`Application init failed: ${err}`);
+    })
