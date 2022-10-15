048b2dc3e8c2e317894db0e550a2963d4d338819
unknown <craxwar@gmail.com>
2022-10-15 17:52:01 +0300

    Adding edit delete and booking views and funcion

--- JS Back-End exam preparation/JS Back-End Retake - 16 December2020/controllers/hotelController.js
+++ JS Back-End exam preparation/JS Back-End Retake - 16 December2020/controllers/hotelController.js
@@ -1,12 +1,21 @@
-const { create } = require('../services/hotelService');
+const { create, getById, update, deleteById, bookRoom } = require('../services/hotelService')
 const { parseError } = require('../util/parser');
 
 const hotelController = require('express').Router();
 
 
-hotelController.get('/:id/details', (req, res) => {
+hotelController.get('/:id/details', async (req, res) => {
+    const hotel = await getById(req.params.id);
+
+    if (hotel.owner == req.user._id) {
+        hotel.isOwner = true;
+    } else if (hotel.bookings.map(b => b.toString()).includes(req.user._id.toString())) {
+        hotel.isBooked = true;
+    }
+
     res.render('details', {
-        title: 'Hotel Details'
+        title: 'Hotel Details',
+        hotel
     });
 });
 
@@ -40,10 +49,79 @@ hotelController.post('/create', async (req, res) => {
     }
 });
 
-hotelController.get('/:id/edit', (req, res) => {
+hotelController.get('/:id/edit', async (req, res) => {
+    const hotel = await getById(req.params.id);
+
+    if (hotel.owner != req.user._id) {
+        return res.redirect('/auth/login')
+    }
+
     res.render('edit', {
-        title: 'Edit Hotel'
+        title: 'Edit Hotel',
+        hotel
     });
+});
+
+hotelController.post('/:id/edit', async (req, res) => {
+    const hotel = await getById(req.params.id);
+
+    if (hotel.owner != req.user._id) {
+        return res.redirect('/auth/login')
+    }
+
+    const edited = {
+        name: req.body.name,
+        city: req.body.city,
+        imageUrl: req.body.imageUrl,
+        rooms: Number(req.body.rooms),
+    };
+
+    try {
+        if (Object.values(edited).some(v => !v)) {
+            throw new Error('All fields are required!');
+        }
+
+        await update(req.params.id, edited);
+        res.redirect(`/hotel/${req.params.id}/details`);
+
+    } catch (err) {
+        res.render('edit', {
+            title: 'Edit Hotel',
+            hotel: Object.assign(edited, { _id: req.params.id }),
+            errors: parseError(err)
+        });
+    }
+});
+
+hotelController.get('/:id/delete', async (req, res) => {
+    const hotel = await getById(req.params.id);
+
+    if (hotel.owner != req.user._id) {
+        return res.redirect('/auth/login');
+    }
+
+    await deleteById(req.params.id);
+    res.redirect('/');
+});
+
+hotelController.get('/:id/book', async (req, res) => {
+    const hotel = await getById(req.params.id);
+
+    try {
+        if (hotel.owner == req.user._id) {
+            hotel.isOwner = true;
+            throw new Error ('Cannot book your own hotel!');
+        }
+    
+        await bookRoom(req.params.id, req.user._id);
+        res.redirect(`/hotel/${req.params.id}/details`);
+    } catch (err) {
+        res.render('details', {
+            title: 'Hotel Details',
+            hotel,
+            errors: parseError(err)
+        });
+    }
 });
 
 module.exports = hotelController;
--- JS Back-End exam preparation/JS Back-End Retake - 16 December2020/services/hotelService.js
+++ JS Back-End exam preparation/JS Back-End Retake - 16 December2020/services/hotelService.js
@@ -1,11 +1,11 @@
 const Hotel = require("../models/Hotel");
 
 async function getAll() {
-
+    return Hotel.find({}).lean();
 }
 
 async function getById(id) {
-
+    return Hotel.findById(id).lean();
 }
 
 async function create(hotel) {
@@ -13,15 +13,33 @@ async function create(hotel) {
 }
 
 async function update(id, hotel) {
+    const exist = await Hotel.findById(id);
 
+    exist.name = hotel.name;
+    exist.city = hotel.city;
+    exist.imageUrl = hotel.imageUrl;
+    exist.rooms = hotel.rooms;
+
+    await exist.save();
 }
 
 async function deleteById(id) {
-
+    await Hotel.findByIdAndRemove(id);
 }
 
 async function bookRoom(hotelId, userId) {
+    const hotel = await Hotel.findById(hotelId);
 
+    if (hotel.bookings.includes(userId)) {
+        throw new Error('Cannot book same hotel twice')
+    }
+
+    hotel.bookings.push(userId);
+    await hotel.save();
+}
+
+async function getByUserBooking(userId) {
+    return (await Hotel.find({ bookings: userId }).lean()).map(h => h.name);
 }
 
 module.exports = {
@@ -30,5 +48,6 @@ module.exports = {
     create,
     update,
     deleteById,
-    bookRoom
+    bookRoom,
+    getByUserBooking
 };
