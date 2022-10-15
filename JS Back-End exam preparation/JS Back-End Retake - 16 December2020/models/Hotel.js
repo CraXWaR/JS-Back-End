a9cf212ac7ae9bd786ef0b356f3357b168f9c2a1
unknown <craxwar@gmail.com>
2022-10-15 15:33:19 +0300

    Addig create functionality

---
+++ JS Back-End exam preparation/JS Back-End Retake - 16 December2020/models/Hotel.js
@@ -0,0 +1,54 @@
+const { Schema, model, Types} = require('mongoose');
+
+
+
+const URL_PATTERN = /^https?:\/\/.+$/i;
+
+const hotelSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [4, 'Hotel name must be at least 4 characters long']
+    },
+    city: {
+        type: String,
+        required: true,
+        minlength: [4, 'City name must be at least 3 characters long']
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => URL_PATTERN.test(value),
+            message: 'Image URL is not valid'
+        }
+    },
+    rooms: {
+        type: Number,
+        required: true,
+        min: [1, 'Hotel must have at least 1 free room!'],
+        max: [100, 'Hotel can\'t have more than 100 free rooms!']
+    },
+    bookings: {
+        type: [Types.ObjectId],
+        ref: 'User',
+        default: []
+    },
+    owner: {
+        type: Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+})
+
+hotelSchema.index({ name: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
+
+const Hotel = model('Hotel', hotelSchema);
+
+module.exports = Hotel;
