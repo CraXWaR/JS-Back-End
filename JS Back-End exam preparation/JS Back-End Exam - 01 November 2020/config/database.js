676bbcd23a1e2da24dd7fbdf05676b7f13c8e420
unknown <craxwar@gmail.com>
2022-10-18 12:26:28 +0300

    Adding real assigment files

---
+++ JS Back-End exam preparation/JS Back-End Exam - 01 November 2020/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+//TODO change data base
+const CONNECTION_STRING = 'mongodb://0.0.0.0:27017/softTutorials';
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('Database connected');
+        
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
---
+++ JS Back-End exam preparation/JS Back-End Exam - 01 November 2020/models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+
+//todo add user propertiest and validation on task
+const userSchema = new Schema({
+    username: { type: String, 
+        required: true, 
+        unique: true, 
+        minlength: [5, 'At least 5 characters'] 
+    },
+    hashedPassword: { type: String, 
+        required: true 
+    },
+});
+
+userSchema.index({ username: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
+
+const User = model('User', userSchema);
+
+
+module.exports = User;
