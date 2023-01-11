c61b4e07b0914e29fecb53034b8b3af70191b5d1
unknown <craxwar@gmail.com>
2023-01-11 13:43:57 +0200

    Removing cat shelter

--- Cat Shelter - Workshop/handlers/index.js
+++
@@ -1,8 +0,0 @@
-const homeHandler = require('./home');
-const staticFiles = require('./static-files');
-const catHandler = reuqire('./cat')
-module.exports = [
-    homeHandler,
-    staticFiles,
-    catHandler
-];
